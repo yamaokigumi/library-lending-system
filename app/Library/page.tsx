29c14d07dce26441ef843858608dfd9086f4bf59
SFjapan <fs12265669@gmail.com>
2024-05-16 09:47:54 +0900

    main画面仮

---
+++ app/Library/page.tsx
@@ -0,0 +1,153 @@
+import Image from "next/image"
+export default function Library(){
+    return(
+        <main className="min-h-screen">
+            <div className="fixed top-0 left-0 w-full z-10">
+                <div className="bg-[#3BDEFF] w-full h-10 items-center text-center justify-center"><p>ECC図書館 </p></div>
+                <div className="bg-red-500 w-full h-10 text-center justify-items-center">
+                    <input type="text" placeholder="本のタイトル"/> 
+                </div>
+            </div>
+            <div className="flex flex-row overflow-y-hidden">
+                <div className="w-1/2 bg-[#FFFAEB] h-screen fixed top-20 text-center text-justify">#JAVA</div>
+                <div className="w-1/2 bg-white grid grid-cols-2 relative left-1/2 top-20 z-0">
+                    <div className="block items-center content-center mx-auto justify-center text-center">
+                        <Image 
+                            src={"https://www.svgrepo.com/show/533406/book.svg"}
+                            width={200}
+                            height={200}
+                            alt="book"
+                        />
+                        本
+                    </div>
+                    <div className="block items-center content-center mx-auto justify-center text-center">
+                        <Image 
+                            src={"https://www.svgrepo.com/show/533406/book.svg"}
+                            width={200}
+                            height={200}
+                            alt="book"
+                        />
+                        本
+                    </div>
+                    <div className="block items-center content-center mx-auto justify-center text-center">
+                        <Image 
+                            src={"https://www.svgrepo.com/show/533406/book.svg"}
+                            width={200}
+                            height={200}
+                            alt="book"
+                        />
+                        本
+                    </div>
+                    <div className="block items-center content-center mx-auto justify-center text-center">
+                        <Image 
+                            src={"https://www.svgrepo.com/show/533406/book.svg"}
+                            width={200}
+                            height={200}
+                            alt="book"
+                        />
+                        本
+                    </div>
+                    <div className="block items-center content-center mx-auto justify-center text-center">
+                        <Image 
+                            src={"https://www.svgrepo.com/show/533406/book.svg"}
+                            width={200}
+                            height={200}
+                            alt="book"
+                        />
+                        本
+                    </div>
+                    <div className="block items-center content-center mx-auto justify-center text-center">
+                        <Image 
+                            src={"https://www.svgrepo.com/show/533406/book.svg"}
+                            width={200}
+                            height={200}
+                            alt="book"
+                        />
+                        本
+                    </div>
+                    <div className="block items-center content-center mx-auto justify-center text-center">
+                        <Image 
+                            src={"https://www.svgrepo.com/show/533406/book.svg"}
+                            width={200}
+                            height={200}
+                            alt="book"
+                        />
+                        本
+                    </div>
+                    <div className="block items-center content-center mx-auto justify-center text-center">
+                        <Image 
+                            src={"https://www.svgrepo.com/show/533406/book.svg"}
+                            width={200}
+                            height={200}
+                            alt="book"
+                        />
+                        本
+                    </div>
+                    <div className="block items-center content-center mx-auto justify-center text-center">
+                        <Image 
+                            src={"https://www.svgrepo.com/show/533406/book.svg"}
+                            width={200}
+                            height={200}
+                            alt="book"
+                        />
+                        本
+                    </div>
+                    <div className="block items-center content-center mx-auto justify-center text-center">
+                        <Image 
+                            src={"https://www.svgrepo.com/show/533406/book.svg"}
+                            width={200}
+                            height={200}
+                            alt="book"
+                        />
+                        本
+                    </div>
+                    <div className="block items-center content-center mx-auto justify-center text-center">
+                        <Image 
+                            src={"https://www.svgrepo.com/show/533406/book.svg"}
+                            width={200}
+                            height={200}
+                            alt="book"
+                        />
+                        本
+                    </div>
+                    <div className="block items-center content-center mx-auto justify-center text-center">
+                        <Image 
+                            src={"https://www.svgrepo.com/show/533406/book.svg"}
+                            width={200}
+                            height={200}
+                            alt="book"
+                        />
+                        本
+                    </div>
+                    <div className="block items-center content-center mx-auto justify-center text-center">
+                        <Image 
+                            src={"https://www.svgrepo.com/show/533406/book.svg"}
+                            width={200}
+                            height={200}
+                            alt="book"
+                        />
+                        本
+                    </div>
+                    <div className="block items-center content-center mx-auto justify-center text-center">
+                        <Image 
+                            src={"https://www.svgrepo.com/show/533406/book.svg"}
+                            width={200}
+                            height={200}
+                            alt="book"
+                        />
+                        本
+                    </div>
+                    <div className="block items-center content-center mx-auto justify-center text-center">
+                        <Image 
+                            src={"https://www.svgrepo.com/show/533406/book.svg"}
+                            width={200}
+                            height={200}
+                            alt="book"
+                        />
+                        本
+                    </div>
+                </div>
+            </div>
+        </main>
+    )
+}
