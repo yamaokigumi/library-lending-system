23245ff970c946dd4cf00d1ac7308ade95a1be8f
SFjapan <fs12265669@gmail.com>
2024-05-23 10:37:03 +0900

    検索モダル作成

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images:{
-        domains:["wikieducator.org","www.cmoa.jp","dosbg3xlm0x1t.cloudfront.net"]
+        domains:[
+            "wikieducator.org",
+            "www.cmoa.jp",
+            "dosbg3xlm0x1t.cloudfront.net",
+            "m.media-amazon.com"
+        ]
     }
 }
 
